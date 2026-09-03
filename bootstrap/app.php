e6de20c73c0b5f51ba34e314f4121c60699ef869
UniqueGinun <uniqueginun@gmail.com>
2026-09-04 00:28:53 +0300

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Http\Request;
 use MinsentSdk\MiniSentry\Facades\MiniSentry;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
